a9e9b010f74cf8f06596b8bf1d6d6be557ecb695
domitoribuotoko <dmitriy.gdubinin@gmail.com>
2023-05-24 18:39:20 +0400

    initial_default ui

---
+++ android/app/src/main/kotlin/com/example/tz_nopreset_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tz_nopreset_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/base/app_constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors{
+  Color mainColor = const Color(0xFF0f1c25);
+}
+
+final appColors= AppColors();
